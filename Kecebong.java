dcf48d3a4cdf961e34063b7281f111b7d5459c0d
Cinderella-Ih-Hsin <100737406+Cinderella-Ih-Hsin@users.noreply.github.com>
2022-10-20 20:36:31 +0700

    Add files via upload

---
+++ Kecebong.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package katak;
+
+/**
+ *
+ * @author Cinderella
+ */
+public class Kecebong extends Katak {
+    double panjangEkor;
+    public Kecebong(int umur, String nama, double panjangEkor){
+        super(umur,nama);
+        this.panjangEkor=panjangEkor;
+     }
+}
