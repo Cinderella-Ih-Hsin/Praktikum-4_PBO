c167d7ee307519190e3ef2ede9da1c801e40fa2e
Cinderella-Ih-Hsin <100737406+Cinderella-Ih-Hsin@users.noreply.github.com>
2022-10-20 20:35:20 +0700

    Add files via upload

---
+++ Orng.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum4;
+
+/**
+ *
+ * @author Cinderella
+ */
+public class Orng {
+    protected String nama;
+    protected int umur;
+    
+    public void Orang (String nama){
+        this.nama=nama;
+    }
+    public void Orang (String nama, int umur){
+        this.nama=nama;
+        this.umur=umur;
+    }
+}
