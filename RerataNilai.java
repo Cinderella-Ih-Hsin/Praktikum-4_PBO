7b1861c1347575bed727049840aae927bc0d4578
Cinderella-Ih-Hsin <100737406+Cinderella-Ih-Hsin@users.noreply.github.com>
2022-10-20 20:35:47 +0700

    Add files via upload

---
+++ RerataNilai.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prak4;
+
+/**
+ *
+ * @author Cinderella
+ */
+public class RerataNilai {
+    public int average(int a, int b){
+        return(a+b)/2;
+    }
+    public double average(double a, double b){
+        return (a+b)/2;
+    }
+    public int average(int a, int b, int c){
+        return (a+b+c)/3;
+    }
+}
